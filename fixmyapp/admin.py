0b8767009cd1328a8e52a9c15ee3f530adedfc79
Stefan Freudenberg <stefan@agaric.com>
2018-03-16 08:13:25 +0100

    Register custom admin class for WorldBorder model

--- fixmyapp/admin.py
+++ fixmyapp/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import WorldBorder
 
-# Register your models here.
+
+class WorldBorderAdmin(admin.OSMGeoAdmin):
+    list_display = ['name', 'pop2005']
+    fields = ['name', 'pop2005', 'mpoly']
+
+admin.site.register(WorldBorder, WorldBorderAdmin)

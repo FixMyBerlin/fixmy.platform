232084551882cc3e0ffed7f762688857ff60417f
Stefan Freudenberg <stefan@agaric.com>
2018-03-23 11:55:18 +0100

    Have view return projects as FeatureCollection objects

--- fixmyapp/views.py
+++ fixmyapp/views.py
@@ -1,11 +1,23 @@
+from django.contrib.gis.db.models.functions import AsGeoJSON
 from django.http import JsonResponse
 from django.core.serializers import serialize
-from .models import Kanten
+from .models import Kanten, Project
+import json
 
 
 def api(request):
-    return JsonResponse(serialize(
-        'geojson',
-        Kanten.objects.all()[0:50],
-        geometry_field='geom',
-        fields=('str_name',)), safe=False)
+    projects = Project.objects.all()
+    result = []
+
+    for p in projects:
+        features = {
+            'type': 'FeatureCollection',
+            'features': [json.loads(e.json) for e in p.edges.annotate(json=AsGeoJSON('geom')).all()],
+            'properties': {
+                'name': p.name,
+                'description': p.description
+            }
+        }
+        result.append(features)
+
+    return JsonResponse(result, safe=False)

9b0a11fc3e395caf1ea83712a1316a46f29fe935
Stefan Freudenberg <stefan@agaric.com>
2018-03-23 10:28:11 +0100

    Customize admin models

--- fixmyapp/admin.py
+++ fixmyapp/admin.py
@@ -1,5 +1,18 @@
 from django.contrib.gis import admin
 from .models import Kanten, Project
 
-admin.site.register(Kanten, admin.OSMGeoAdmin)
-admin.site.register(Project, admin.OSMGeoAdmin)
+
+class ProjectAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('edges',)
+
+
+
+class KantenAdmin(admin.OSMGeoAdmin):
+    search_fields = ('elem_nr',)
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(Kanten, KantenAdmin)
+admin.site.register(Project, ProjectAdmin)

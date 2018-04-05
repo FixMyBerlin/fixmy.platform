86b3d226c3e495adde0d868ce9e4d428e55bfba2
Stefan Freudenberg <stefan@agaric.com>
2018-04-05 21:03:09 +0200

    Move common test code to setUp method

--- fixmyapp/tests.py
+++ fixmyapp/tests.py
@@ -1,47 +1,16 @@
-from django.contrib.gis.geos import MultiLineString
+from django.contrib.gis.geos import LineString, MultiLineString
 from django.test import TestCase
 from .models import Edge, Project
 
 
 class ProjectTests(TestCase):
 
-    def test_geometry_hash_is_saved(self):
-        project = Project.objects.create(name='Lorem ipsum')
-        self.assertIsNone(project.geom_hash)
-
-        edge = Edge.objects.create(
-            objectid=3001,
-            gml_id='re_vms_detailnetz_str.9999',
-            spatial_name='9999',
-            spatial_alias='Mariendorfer Damm',
-            spatial_type='LineString',
-            elem_nr='45460003_45460015.01',
-            strschl='02959',
-            str_name='Mariendorfer Damm',
-            str_bez='B96',
-            strklasse1='II',
-            strklasse='B',
-            strklasse2='STRA',
-            vricht='B',
-            bezirk='Tempelhof-Schöneberg',
-            stadtteil='Mariendorf',
-            ebene='0',
-            von_vp='45460003',
-            bis_vp='45460015',
-            laenge=20100101,
-            gilt_von=20100101,
-            okstra_id='787C5A383D0B434E88FFA2D60EDA90BC',
-            geom=MultiLineString())
-        project.edges.add(edge)
-        project.save()
-        self.assertIsNotNone(project.geom_hash)
-
-
-    def test_has_updated_edges(self):
-        project = Project.objects.create(name='Lorem ipsum')
+    def setUp(self):
+        self.project = Project.objects.create(name='Lorem ipsum')
+        self.edges = []
 
         for i in range(3):
-            edge = Edge.objects.create(
+            self.edges.append(Edge.objects.create(
                 objectid=i,
                 gml_id='re_vms_detailnetz_str.9999',
                 spatial_name='9999',
@@ -63,16 +32,25 @@ class ProjectTests(TestCase):
                 laenge=20100101,
                 gilt_von=20100101,
                 okstra_id='787C5A383D0B434E88FFA2D60EDA90BC',
-                geom=MultiLineString())
-            project.edges.add(edge)
+                geom=MultiLineString()
+            ))
 
-        project.save()
+    def test_geometry_hash_is_saved(self):
+        self.assertIsNone(self.project.geom_hash)
+        self.project.edges.add(self.edges[0])
+        self.project.save()
+        self.assertIsNotNone(self.project.geom_hash)
 
-        self.assertFalse(project.has_updated_edges())
+    def test_has_updated_edges(self):
+        for e in self.edges:
+            self.project.edges.add(e)
+
+        self.project.save()
+
+        self.assertFalse(self.project.has_updated_edges())
 
         edge = Edge.objects.get(pk=1)
         edge.str_bez = 'B97'
         edge.save()
 
-        self.assertTrue(project.has_updated_edges())
-
+        self.assertTrue(self.project.has_updated_edges())

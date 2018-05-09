36563bc97445c733ac6495bd95a6a2becdeb4b6d
Stefan Freudenberg <stefan@agaric.com>
2018-05-09 08:44:01 +0200

    Replace literal value for STATIC_URL with environment variable

--- fixmydjango/settings.py
+++ fixmydjango/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = 'http://storage.googleapis.com/fixmyberlin-201008/static/'
+STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
 STATIC_ROOT = '/code/static'

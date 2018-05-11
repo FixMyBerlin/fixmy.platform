3de933a5ae94b0e79c1cc4231046e36cca220b54
Stefan Freudenberg <stefan@agaric.com>
2018-05-11 13:42:05 +0200

    Add view returning planning sections in progress

--- fixmyapp/urls.py
+++ fixmyapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import planning_sections
+from .views import planning_sections, planning_sections_in_progress
 
 
 urlpatterns = [
@@ -7,5 +7,10 @@ urlpatterns = [
         'planning-sections',
         planning_sections,
         name='planning-sections'
-    )
+    ),
+    path(
+        'planning-sections/in-progress',
+        planning_sections_in_progress,
+        name='planning-sections-in-progress'
+    ),
 ]
--- fixmyapp/views.py
+++ fixmyapp/views.py
@@ -11,15 +11,47 @@ def planning_sections(request):
     }
 
     for p in PlanningSection.objects.all():
-        geometry = p.edges.aggregate(Union('geom'))['geom__union']
+        geometry = p.edges.aggregate(Union('geom'))['geom__union'].merged
         feature = {
             'type': 'Feature',
             'geometry': json.loads(geometry.json),
             'properties': {
+                'id': p.pk,
                 'name': p.name,
-                'description': p.description
+                'progress': p.progress,
             }
         }
         result['features'].append(feature)
+
+    return JsonResponse(result)
+
+
+def planning_sections_in_progress(request):
+    result = {
+        'type': 'FeatureCollection',
+        'features': []
+    }
+
+    for p in PlanningSection.objects.filter(progress__gt=0):
+        geometry = p.edges.aggregate(Union('geom'))['geom__union'].merged
+        feature = {
+            'type': 'Feature',
+            'geometry': json.loads(geometry.json),
+            'properties': {
+                'id': p.pk,
+                'name': p.name,
+                'progress': p.progress,
+                'side': 0
+            }
+        }
+        result['features'].append(feature)
+        center = {
+            'type': 'Feature',
+            'geometry': json.loads(geometry.centroid.json),
+            'properties': {
+                'id': p.pk
+            }
+        }
+        result['features'].append(center)
 
     return JsonResponse(result)

60eef3c13553c69127e93d8424ab9ffd614c8fec
Maik Hoepfel <m@maikhoepfel.de>
2018-05-29 10:19:56 +0200

    Heroku skeleton

--- fixmydjango/settings.py
+++ fixmydjango/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -133,3 +135,8 @@ STATIC_ROOT = '/code/static'
 
 
 SITE_ID = 1
+
+# Activate Django-Heroku.
+# https://devcenter.heroku.com/articles/django-app-configuration
+django_heroku.settings(locals())
+

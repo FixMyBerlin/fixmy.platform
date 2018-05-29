e8a4e51d37e0a2a7d5b7848e5867262b44e833bc
Maik Hoepfel <m@maikhoepfel.de>
2018-05-29 15:36:13 +0200

    Heroku fix

--- fixmydjango/settings.py
+++ fixmydjango/settings.py
@@ -145,8 +145,8 @@ SITE_ID = 1
 django_heroku.settings(locals())
 
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgdal.so"
+GEOS_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgeos_c.so"
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [

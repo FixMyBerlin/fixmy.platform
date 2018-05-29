98fd4f23f2a000e9028282360c31711fbd85595a
Maik Hoepfel <m@maikhoepfel.de>
2018-05-29 15:45:26 +0200

    Allow everyone to create a user

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+
 from .serializers import UserSerializer
 
 
@@ -7,3 +9,4 @@ class UserCreate(CreateAPIView):
     Lets you create a new user
     """
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)

20e46bd46d645a96bdb08dfad9e13782f1a58810
Stefan Freudenberg <stefan@agaric.com>
2018-06-20 14:23:32 +0200

    Customize logging onfiguration

--- fixmydjango/settings.py
+++ fixmydjango/settings.py
@@ -13,6 +13,25 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import django_heroku
 import os
 
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

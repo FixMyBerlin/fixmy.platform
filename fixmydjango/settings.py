add569b3d7b1b5b64813b3155bec9c0eb9393a7b
Stefan Freudenberg <stefan@agaric.com>
2018-06-20 12:00:49 +0200

    Activate Heroku

--- fixmydjango/settings.py
+++ fixmydjango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -133,3 +134,9 @@ STATIC_ROOT = '/code/static'
 
 
 SITE_ID = 1
+
+
+# Activate Django-Heroku
+# https://devcenter.heroku.com/articles/django-app-configuration
+
+django_heroku.settings(locals())

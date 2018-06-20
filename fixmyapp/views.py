01f3532c9f3816386268b6ee3d26c07cdffb4288
Stefan Freudenberg <stefan@agaric.com>
2018-06-20 07:08:09 +0200

    Fix PEP8 violations

--- fixmyapp/views.py
+++ fixmyapp/views.py
@@ -4,6 +4,7 @@ from .models import PlanningSection
 import json
 import random
 
+
 def planning_sections(request):
     result = {
         'type': 'FeatureCollection',
@@ -19,9 +20,9 @@ def planning_sections(request):
                 'id': p.pk,
                 'name': p.name,
                 'side0_progress': p.progress,
-                'side0_index': round(random.randint(5,50)*0.1, 1),
+                'side0_index': round(random.randint(5, 50) * 0.1, 1),
                 'side1_progress': p.progress,
-                'side1_index': round(random.randint(5,50)*0.1, 1)
+                'side1_index': round(random.randint(5, 50) * 0.1, 1)
             }
         }
         result['features'].append(feature)

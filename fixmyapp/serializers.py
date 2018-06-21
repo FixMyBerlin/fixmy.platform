09b6f9cc1d9c63010f8ea5f9f15bdbb3969346d8
Stefan Freudenberg <stefan@agaric.com>
2018-06-21 11:29:13 +0200

    Allow client to set id of Profile instance

--- fixmyapp/serializers.py
+++ fixmyapp/serializers.py
@@ -3,6 +3,8 @@ from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField()
+
     class Meta:
         model = Profile
         fields = '__all__'

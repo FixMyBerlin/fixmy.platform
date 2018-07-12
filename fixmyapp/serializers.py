d64599b32913fd44b22518545aab79daa6c98124
Stefan Freudenberg <stefan@agaric.com>
2018-07-12 10:30:42 +0200

    Add serializers for PlanningSection and PlanningSectionDetails

--- fixmyapp/serializers.py
+++ fixmyapp/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import PlanningSection, PlanningSectionDetails, Profile
+
+
+class PlanningSectionDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlanningSectionDetails
+        fields = (
+            'side',
+            'orientation',
+            'length',
+            'crossings',
+        )
+
+
+class PlanningSectionSerializer(serializers.ModelSerializer):
+    details = PlanningSectionDetailsSerializer(many=True)
+
+    class Meta:
+        model = PlanningSection
+        fields = ('id', 'name', 'description', 'details')
 
 
 class ProfileSerializer(serializers.ModelSerializer):

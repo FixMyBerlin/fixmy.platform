866158a0747c5e82126cca91df9ffe72e9540fbe
Stefan Freudenberg <stefan@agaric.com>
2018-08-06 11:24:37 +0200

    Add handling of missing edges in planningsection import

--- fixmyapp/management/commands/importplanningsections.py
+++ fixmyapp/management/commands/importplanningsections.py
@@ -29,6 +29,10 @@ class Command(BaseCommand):
                 pk=row['MetaID']
             )
             obj.name = row['Straßen Name']
-            obj.edges.add(Edge.objects.get(pk=row['ElemNr']))
+            try:
+                obj.edges.add(Edge.objects.get(pk=row['ElemNr']))
+            except Edge.DoesNotExist as e:
+                message = 'Referenced edge with ElemNr {} does not exist.'.format(row['ElemNr'])
+                self.stderr.write(message)
             obj.geom_hash = obj.compute_geom_hash()
             obj.save()

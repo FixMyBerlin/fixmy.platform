13d040721a6517e266ca170ad8b9f945fd3b3378
Stefan Freudenberg <stefan@agaric.com>
2018-08-21 16:59:45 +0200

    Enable gzip compression

--- fixmydjango/settings.py
+++ fixmydjango/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',

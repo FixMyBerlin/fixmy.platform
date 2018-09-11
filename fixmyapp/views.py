603a583f34e8bb799a1ed61be2e60a30422d2242
Stefan Freudenberg <stefan@agaric.com>
2018-09-11 09:00:22 +0200

    Increase max_page_size for list views

--- fixmyapp/views.py
+++ fixmyapp/views.py
@@ -15,7 +15,7 @@ from .serializers import (
 
 
 class DefaultPagination(PageNumberPagination):
-    max_page_size = 100
+    max_page_size = 1000
     page_size = 10
     page_size_query_param = 'page_size'
 

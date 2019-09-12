7f10e145c8490ba9f1f5bfe3e4b487447fe2b027
Stefan Freudenberg <stefan@agaric.com>
2019-09-12 09:18:11 +0200

    Disable HTMLRenderer

--- fixmydjango/settings.py
+++ fixmydjango/settings.py
@@ -244,7 +244,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
 
 

04c8a716ae1fb1f8577df64765d8d92f477f724a
Stefan Freudenberg <stefan@agaric.com>
2019-09-20 07:50:24 +0200

    Add commands for exporting sections and projects to GeoJSON (#4)

---
+++ fixmyapp/management/commands/exportsections.py
@@ -0,0 +1,52 @@
+from django.core.management.base import BaseCommand
+from fixmyapp.models import Section
+import argparse
+import json
+
+
+class Command(BaseCommand):
+    help = 'Exports planning sections as GeoJSON'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'file',
+            type=argparse.FileType('w'),
+            help='write to file'
+        )
+        parser.add_argument(
+            '--indent',
+            type=int,
+            default=None,
+            help='indentation level for pretty printing'
+        )
+
+    def handle(self, *args, **options):
+        result = {
+            'type': 'FeatureCollection',
+            'features': []
+        }
+
+        for s in Section.objects.all():
+            feature = {
+                'type': 'Feature',
+                'geometry': json.loads(s.geometry.json),
+                'properties': {
+                    'id': s.pk,
+                    'street_name': s.street_name,
+                    'suffix': s.suffix,
+                    'borough': s.borough,
+                    'street_category': s.street_category,
+                    'velocity': float(round(s.velocity_index(), 3)),
+                    'safety': float(round(s.safety_index(), 3))
+                }
+            }
+
+            for detail in s.details().all():
+                prefix = 'side{}_'.format(detail.side)
+                feature['properties'][prefix + 'orientation'] = detail.orientation
+                feature['properties'][prefix + 'velocity'] = float(round(detail.velocity_index(), 3))
+                feature['properties'][prefix + 'safety'] = float(round(detail.safety_index(), 3))
+
+            result['features'].append(feature)
+
+        json.dump(result, options['file'], indent=options['indent'])

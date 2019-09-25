f9d79271ce70e96e632450b190b460080a492652
Vincent Ahrend <mail@vincentahrend.com>
2019-09-25 12:01:04 +0200

    Revert quote formatting to single quotes

--- fixmyapp/management/commands/updateprojectgeometries.py
+++ fixmyapp/management/commands/updateprojectgeometries.py
@@ -48,7 +48,7 @@ class LayerMapping(django.contrib.gis.utils.LayerMapping):
                     if strict:
                         raise
                     elif not silent:
-                        stream.write("Ignoring feature id: {}\n".format(feat.fid, msg))
+                        stream.write('Ignoring feature id: {}\n'.format(feat.fid, msg))
                 else:
                     # Constructing the model using the keyword args
                     # If we want unique models on a particular field, handle the
@@ -70,27 +70,27 @@ class LayerMapping(django.contrib.gis.utils.LayerMapping):
                         num_saved += 1
                         if verbose:
                             stream.write(
-                                "%s: %s\n" % ("Updated" if is_update else "Saved", m)
+                                '%s: %s\n' % ('Updated' if is_update else 'Saved', m)
                             )
                     except Exception as msg:
                         if strict:
                             # Bailing out if the `strict` keyword is set.
                             if not silent:
                                 stream.write(
-                                    "Failed to save the feature (id: %s) into the "
-                                    "model with the keyword arguments:\n" % feat.fid
+                                    'Failed to save the feature (id: %s) into the '
+                                    'model with the keyword arguments:\n' % feat.fid
                                 )
-                                stream.write("%s\n" % kwargs)
+                                stream.write('%s\n' % kwargs)
                             raise
                         elif not silent:
                             stream.write(
-                                "Failed to save %s:\n %s\nContinuing\n" % (kwargs, msg)
+                                'Failed to save %s:\n %s\nContinuing\n' % (kwargs, msg)
                             )
 
                 # Printing progress information, if requested.
                 if progress and num_feat % progress_interval == 0:
                     stream.write(
-                        "Processed %d features, saved %d ...\n" % (num_feat, num_saved)
+                        'Processed %d features, saved %d ...\n' % (num_feat, num_saved)
                     )
 
             # Only used for status output purposes -- incremental saving uses the
@@ -105,7 +105,7 @@ class LayerMapping(django.contrib.gis.utils.LayerMapping):
             # Incremental saving is requested at the given interval (step)
             if default_range:
                 raise django.contrib.gis.utils.LayerMapError(
-                    "The `step` keyword may not be used in conjunction with the `fid_range` keyword."
+                    'The `step` keyword may not be used in conjunction with the `fid_range` keyword.'
                 )
             beg, num_feat, num_saved = (0, 0, 0)
             indices = range(step, nfeat, step)
@@ -124,7 +124,7 @@ class LayerMapping(django.contrib.gis.utils.LayerMapping):
                     beg = end
                 except Exception:  # Deliberately catch everything
                     stream.write(
-                        "%s\nFailed to save slice: %s\n" % ("=-" * 20, step_slice)
+                        '%s\nFailed to save slice: %s\n' % ('=-' * 20, step_slice)
                     )
                     raise
         else:
@@ -133,36 +133,36 @@ class LayerMapping(django.contrib.gis.utils.LayerMapping):
 
 
 class Command(BaseCommand):
-    help = "Update project geometries"
+    help = 'Update project geometries'
 
     def add_arguments(self, parser):
-        parser.add_argument("file", type=str, help="A shape file")
+        parser.add_argument('file', type=str, help='A shape file')
         parser.add_argument(
-            "type",
-            choices=["multipoint", "linestring"],
-            help="The type of geometries in the shape file",
+            'type',
+            choices=['multipoint', 'linestring'],
+            help='The type of geometries in the shape file',
         )
         parser.add_argument(
-            "--show-progress",
-            action="store_true",
-            dest="progress",
-            help="display the progress bar in any verbosity level.",
+            '--show-progress',
+            action='store_true',
+            dest='progress',
+            help='display the progress bar in any verbosity level.',
         )
 
     def handle(self, *args, **options):
-        mapping = {"project_key": "ProjectKey", "geometry": options["type"]}
+        mapping = {'project_key': 'ProjectKey', 'geometry': options['type']}
         lm = LayerMapping(
             Project,
-            os.path.abspath(options["file"]),
+            os.path.abspath(options['file']),
             mapping,
             transform=True,
-            encoding="utf-8",
-            unique=("project_key",),
+            encoding='utf-8',
+            unique=('project_key',),
         )
         lm.save(
-            verbose=True if options["verbosity"] > 2 else False,
-            progress=options["progress"] or options["verbosity"] > 1,
-            silent=options["verbosity"] == 0,
+            verbose=True if options['verbosity'] > 2 else False,
+            progress=options['progress'] or options['verbosity'] > 1,
+            silent=options['verbosity'] == 0,
             stream=self.stdout,
             strict=True,
         )

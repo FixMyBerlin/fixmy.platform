8d584b27125ac123ffbfe3dcb1bbbcfcceb4d37c
Stefan Freudenberg <stefan@agaric.com>
2019-11-06 15:13:14 +0100

    Add alert date to projects (#53)
    Closes #8

--- fixmyapp/admin.py
+++ fixmyapp/admin.py
@@ -1,5 +1,8 @@
+from datetime import date
+from django.contrib.admin import SimpleListFilter
 from django.contrib.contenttypes.admin import GenericTabularInline
 from django.contrib.gis import admin
+from django.utils.translation import gettext_lazy as _
 from markdownx.admin import MarkdownxModelAdmin
 from reversion.admin import VersionAdmin
 from .models import (
@@ -31,12 +34,45 @@ class PlanningAdmin(VersionAdmin):
     search_fields = ('planning_sections__edges__str_name', 'project_key')
 
 
+class AlertDateFilter(SimpleListFilter):
+    title = _('alert date')
+    parameter_name = 'alert'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('exceeded', _('exceeded')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        if self.value() == 'exceeded':
+            return queryset.filter(alert_date__lte=date.today())
+
+
 class ProjectAdmin(admin.OSMGeoAdmin, VersionAdmin):
     autocomplete_fields = ('faq',)
     inlines = (PhotoInline,)
-    list_display = ('project_key', 'title', 'street_name', 'category', 'phase', 'responsible')
-    list_filter = ('category', 'phase', 'responsible')
-    search_fields = ('project_key',)
+    list_display = (
+        'project_key',
+        'title',
+        'street_name',
+        'category',
+        'phase',
+        'responsible',
+        'alert_date')
+    list_filter = (AlertDateFilter, 'category', 'phase', 'responsible')
+    search_fields = ('project_key', 'street_name')
 
 
 class PlanningSectionDetailsAdmin(admin.ModelAdmin):
--- fixmyapp/models.py
+++ fixmyapp/models.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.fields import (
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.gis.db import models
 from django.contrib.postgres.fields import JSONField
+from django.utils.translation import gettext_lazy as _
 from markdownx.models import MarkdownxField
 import decimal
 import hashlib
@@ -95,11 +96,19 @@ class PlanningSection(BaseModel):
 
 
 class Section(BaseModel):
-    street_name = models.CharField(max_length=100)
-    suffix = models.CharField(blank=True, null=True, max_length=3)
-    borough = models.CharField(blank=True, null=True, max_length=255)
-    street_category = models.PositiveSmallIntegerField(null=True)
-    geometry = models.MultiLineStringField(srid=4326, null=True)
+    street_name = models.CharField(_('street name'), max_length=100)
+    suffix = models.CharField(_('suffix'), blank=True, null=True, max_length=3)
+    borough = models.CharField(
+        _('borough'), blank=True, null=True, max_length=255
+    )
+    street_category = models.PositiveSmallIntegerField(
+        _('street category'), null=True
+    )
+    geometry = models.MultiLineStringField(_('geometry'), srid=4326, null=True)
+
+    class Meta:
+        verbose_name = _('section')
+        verbose_name_plural = _('sections')
 
     def velocity_index(self):
         if len(self.details.all()) > 0:
@@ -121,19 +130,25 @@ class Question(BaseModel):
     text = models.CharField(max_length=256)
     answer = MarkdownxField()
 
+    class Meta:
+        verbose_name = _('question')
+        verbose_name_plural = _('questions')
+        ordering = ('text',)
+
     def __str__(self):
         return self.text
-
-    class Meta:
-        ordering = ('text',)
 
 
 class Photo(BaseModel):
     content_object = GenericForeignKey('content_type', 'object_id')
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    copyright = models.CharField(blank=True, null=True, max_length=256)
+    copyright = models.CharField(_('copyright'), blank=True, null=True, max_length=256)
     object_id = models.PositiveIntegerField()
-    src = models.ImageField(upload_to='photos', verbose_name='File')
+    src = models.ImageField(upload_to='photos', verbose_name=_('file'))
+
+    class Meta:
+        verbose_name = _('photo')
+        verbose_name_plural = _('photos')
 
     def __str__(self):
         return self.src.name.split('/')[-1]
@@ -383,33 +398,33 @@ class SectionDetails(BaseModel):
     RIGHT = 0
     LEFT = 1
     SIDE_CHOICES = (
-        (RIGHT, 'right'),
-        (LEFT, 'left'),
+        (RIGHT, _('right')),
+        (LEFT, _('left')),
     )
     NORTH = 'N'
     EAST = 'E'
     SOUTH = 'S'
     WEST = 'W'
     ORIENTATION_CHOICES = (
-        (NORTH, 'north'),
-        (EAST, 'east'),
-        (SOUTH, 'south'),
-        (WEST, 'west'),
+        (NORTH, _('north')),
+        (EAST, _('east')),
+        (SOUTH, _('south')),
+        (WEST, _('west')),
     )
     AVG_WIDTH_CROSSINGS = 6
     CI_RATIO_MIN = 0.65
 
     section = models.ForeignKey(
         Section, related_name='details', on_delete=models.CASCADE)
-    side = models.PositiveSmallIntegerField(choices=SIDE_CHOICES)
-    speed_limit = models.PositiveSmallIntegerField()
-    daily_traffic = models.DecimalField(max_digits=8, decimal_places=2)
-    daily_traffic_heavy = models.DecimalField(max_digits=8, decimal_places=2)
-    daily_traffic_cargo = models.DecimalField(max_digits=8, decimal_places=2)
-    daily_traffic_bus = models.DecimalField(max_digits=8, decimal_places=2)
-    length = models.DecimalField(max_digits=8, decimal_places=2)
-    crossings = models.PositiveSmallIntegerField()
-    orientation = models.CharField(max_length=1, choices=ORIENTATION_CHOICES)
+    side = models.PositiveSmallIntegerField(_('side'), choices=SIDE_CHOICES)
+    speed_limit = models.PositiveSmallIntegerField(_('speed limit'))
+    daily_traffic = models.DecimalField(_('daily traffic'), max_digits=8, decimal_places=2)
+    daily_traffic_heavy = models.DecimalField(_('daily traffic heavy'), max_digits=8, decimal_places=2)
+    daily_traffic_cargo = models.DecimalField(_('daily traffic cargo'), max_digits=8, decimal_places=2)
+    daily_traffic_bus = models.DecimalField(_('daily traffic bus'), max_digits=8, decimal_places=2)
+    length = models.DecimalField(_('length'), max_digits=8, decimal_places=2)
+    crossings = models.PositiveSmallIntegerField(_('crossings'))
+    orientation = models.CharField(_('orientation'), max_length=1, choices=ORIENTATION_CHOICES)
     rva1 = models.DecimalField(max_digits=16, decimal_places=12)
     rva2 = models.DecimalField(max_digits=16, decimal_places=12)
     rva3 = models.DecimalField(max_digits=16, decimal_places=12)
@@ -426,8 +441,8 @@ class SectionDetails(BaseModel):
     photos = GenericRelation(Photo)
 
     class Meta:
-        verbose_name = 'Section details'
-        verbose_name_plural = 'Section details'
+        verbose_name = _('Section details')
+        verbose_name_plural = _('Section details')
 
     def happy_bike_index(self):
         return self.velocity_index() + self.safety_index()
@@ -738,16 +753,16 @@ class Project(BaseModel):
     CATEGORY_MISCELLANEOUS = 'miscellaneous'
 
     CATEGORY_CHOICES = (
-        (CATEGORY_NEW_INFRASTRUCTURE, 'new cycling infrastructure'),
-        (CATEGORY_RENOVATION, 'renovation of cycling infrastructure'),
-        (CATEGORY_BIKE_STREET, 'bike street'),
-        (CATEGORY_MODIFICATION_OF_JUNCTION, 'modification of junction'),
-        (CATEGORY_BIKE_PARKING, 'bike parking'),
-        (CATEGORY_CROSSING_AID, 'crossing aid'),
-        (CATEGORY_MODIFICATION_OF_CROSS_SECTION, 'modification of cross section'),
-        (CATEGORY_NEW_STREET, 'new street'),
-        (CATEGORY_SHARED_SPACE, 'shared space'),
-        (CATEGORY_MISCELLANEOUS, 'miscellaneous'),
+        (CATEGORY_NEW_INFRASTRUCTURE, _('new cycling infrastructure')),
+        (CATEGORY_RENOVATION, _('renovation of cycling infrastructure')),
+        (CATEGORY_BIKE_STREET, _('bike street')),
+        (CATEGORY_MODIFICATION_OF_JUNCTION, _('modification of junction')),
+        (CATEGORY_BIKE_PARKING, _('bike parking')),
+        (CATEGORY_CROSSING_AID, _('crossing aid')),
+        (CATEGORY_MODIFICATION_OF_CROSS_SECTION, _('modification of cross section')),
+        (CATEGORY_NEW_STREET, _('new street')),
+        (CATEGORY_SHARED_SPACE, _('shared space')),
+        (CATEGORY_MISCELLANEOUS, _('miscellaneous')),
     )
 
     PHASE_DRAFT = 'draft'
@@ -759,13 +774,13 @@ class Project(BaseModel):
     PHASE_MISCELLANEOUS = 'miscellaneous'
 
     PHASE_CHOICES = (
-        (PHASE_DRAFT, 'draft'),
-        (PHASE_PLANNING, 'planning'),
-        (PHASE_REVIEW, 'review'),
-        (PHASE_INACTIVE, 'inactive'),
-        (PHASE_EXECUTION, 'execution'),
-        (PHASE_READY, 'ready'),
-        (PHASE_MISCELLANEOUS, 'miscellaneous'),
+        (PHASE_DRAFT, _('draft')),
+        (PHASE_PLANNING, _('planning')),
+        (PHASE_REVIEW, _('review')),
+        (PHASE_INACTIVE, _('inactive')),
+        (PHASE_EXECUTION, _('execution')),
+        (PHASE_READY, _('ready')),
+        (PHASE_MISCELLANEOUS, _('miscellaneous')),
     )
 
     STATUS_UNKNOWN = 'unknown'
@@ -784,54 +799,92 @@ class Project(BaseModel):
     STATUS_CANCELLED = 'cancelled'
 
     STATUS_CHOICES = (
-        (STATUS_UNKNOWN, 'unknown'),
-        (STATUS_IDEA, 'idea'),
-        (STATUS_PRELIMINARY_PLANNING, 'preliminary planning'),
-        (STATUS_BLUEPRINT_PLANNING, 'blueprint planning'),
-        (STATUS_APPROVAL_PLANNING, 'approval planning'),
-        (STATUS_EXAMINATION, 'examination'),
-        (STATUS_EXECUTION_PLANNING, 'execution planning'),
-        (STATUS_PREPARATION_OF_AWARDING, 'preparation of awarding'),
-        (STATUS_AWARDING, 'awarding'),
-        (STATUS_APPLICATION_FOR_CONSTRUCTION_SITE, 'application for construction site'),
-        (STATUS_EXECUTION_OF_CONSTRUCTION_WORK, 'execution of construction work'),
-        (STATUS_READY, 'ready'),
-        (STATUS_REVIEW, 'review'),
-        (STATUS_CANCELLED, 'cancelled'),
+        (STATUS_UNKNOWN, _('unknown')),
+        (STATUS_IDEA, _('idea')),
+        (STATUS_PRELIMINARY_PLANNING, _('preliminary planning')),
+        (STATUS_BLUEPRINT_PLANNING, _('blueprint planning')),
+        (STATUS_APPROVAL_PLANNING, _('approval planning')),
+        (STATUS_EXAMINATION, _('examination')),
+        (STATUS_EXECUTION_PLANNING, _('execution planning')),
+        (STATUS_PREPARATION_OF_AWARDING, _('preparation of awarding')),
+        (STATUS_AWARDING, _('awarding')),
+        (STATUS_APPLICATION_FOR_CONSTRUCTION_SITE, _('application for construction site')),
+        (STATUS_EXECUTION_OF_CONSTRUCTION_WORK, _('execution of construction work')),
+        (STATUS_READY, _('ready')),
+        (STATUS_REVIEW, _('review')),
+        (STATUS_CANCELLED, _('cancelled')),
     )
 
     RIGHT = 0
     LEFT = 1
     BOTH = 2
     SIDE_CHOICES = (
-        (RIGHT, 'right'),
-        (LEFT, 'left'),
-        (BOTH, 'both')
+        (RIGHT, _('right')),
+        (LEFT, _('left')),
+        (BOTH, _('both'))
     )
 
-    published = models.BooleanField(default=True)
-    title = models.CharField(max_length=256)
-    side = models.PositiveSmallIntegerField(choices=SIDE_CHOICES)
-    responsible = models.CharField(max_length=256)
-    description = MarkdownxField()
-    short_description = models.CharField(blank=True, null=True, max_length=200)
-    geometry = models.GeometryField(blank=True, null=True)
-    length = models.IntegerField(blank=True, null=True)
-    category = models.CharField(blank=True, null=True, max_length=40, choices=CATEGORY_CHOICES)
-    project_key = models.CharField(blank=True, null=True, max_length=100)
-    street_name = models.CharField(max_length=100)
-    borough = models.CharField(blank=True, null=True, max_length=255)
-    costs = models.PositiveIntegerField(blank=True, null=True)
-    draft_submitted = models.CharField(blank=True, null=True, max_length=100)
-    construction_started = models.CharField(blank=True, null=True, max_length=100)
-    construction_completed = models.CharField(blank=True, null=True, max_length=100)
-    phase = models.CharField(blank=True, null=True, max_length=30, choices=PHASE_CHOICES)
-    status = models.CharField(blank=True, null=True, max_length=40, choices=STATUS_CHOICES)
-    external_url = models.URLField(blank=True, null=True)
-    cross_section = models.ImageField(upload_to='photos', blank=True, null=True)
-    faq = models.ManyToManyField(Question, blank=True)
+    project_key = models.CharField(
+        _('project key'), max_length=100, unique=True)
+    published = models.BooleanField(_('published'), default=True)
+    title = models.CharField(_('title'), max_length=256)
+    side = models.PositiveSmallIntegerField(_('side'), choices=SIDE_CHOICES)
+    responsible = models.CharField(_('responsible'), max_length=256)
+    description = MarkdownxField(_('description'))
+    short_description = models.CharField(
+        _('short description'), blank=True, null=True, max_length=200
+    )
+    geometry = models.GeometryField(_('geometry'), blank=True, null=True)
+    length = models.IntegerField(_('length'), blank=True, null=True)
+    category = models.CharField(
+        _('category'),
+        blank=True,
+        null=True,
+        max_length=40,
+        choices=CATEGORY_CHOICES)
+    street_name = models.CharField(_('street name'), max_length=100)
+    borough = models.CharField(
+        _('borough'), blank=True, null=True, max_length=255)
+    costs = models.PositiveIntegerField(_('costs'), blank=True, null=True)
+    draft_submitted = models.CharField(
+        _('draft submitted'), blank=True, null=True, max_length=100
+    )
+    construction_started = models.CharField(
+        _('construction started'), blank=True, null=True, max_length=100
+    )
+    construction_completed = models.CharField(
+        _('construction completed'),
+        blank=True,
+        null=True,
+        default=_('unknown'),
+        max_length=100
+    )
+    alert_date = models.DateField(_('alert date'), blank=True, null=True)
+    phase = models.CharField(
+        _('phase'),
+        blank=True,
+        null=True,
+        max_length=30,
+        choices=PHASE_CHOICES
+    )
+    status = models.CharField(
+        _('status'),
+        blank=True,
+        null=True,
+        max_length=40,
+        choices=STATUS_CHOICES
+    )
+    external_url = models.URLField(_('external URL'), blank=True, null=True)
+    cross_section = models.ImageField(
+        _('cross section'), upload_to='photos', blank=True, null=True
+    )
+    faq = models.ManyToManyField(Question, verbose_name=_('faq'), blank=True)
     photos = GenericRelation(Photo)
     likes = GenericRelation(Like)
+
+    class Meta:
+        verbose_name = _('project')
+        verbose_name_plural = _('projects')
 
     def center(self):
         if self.geometry:
@@ -846,9 +899,9 @@ class Profile(BaseModel):
     FEMALE = 'f'
     OTHER = 'o'
     SEX_CHOICES = (
-        (MALE, 'male'),
-        (FEMALE, 'female'),
-        (OTHER, 'other'),
+        (MALE, _('male')),
+        (FEMALE, _('female')),
+        (OTHER, _('other')),
     )
     RACING_CYCLE = 'racing_cycle'
     CITY_BIKE = 'city_bike'
@@ -857,35 +910,52 @@ class Profile(BaseModel):
     CARGO_BIKE = 'cargo_bike'
     E_CARGO_BIKE = 'e_cargo_bike'
     CATEGORY_OF_BIKE_CHOICES = (
-        (RACING_CYCLE, 'racing cycle'),
-        (CITY_BIKE, 'city bike'),
-        (MOUNTAIN_BIKE, 'mountain bike'),
-        (E_BIKE, 'e-bike'),
-        (CARGO_BIKE, 'cargo bike'),
-        (E_CARGO_BIKE, 'e-cargo-bike'),
+        (RACING_CYCLE, _('racing cycle')),
+        (CITY_BIKE, _('city bike')),
+        (MOUNTAIN_BIKE, _('mountain bike')),
+        (E_BIKE, _('e-bike')),
+        (CARGO_BIKE, _('cargo bike')),
+        (E_CARGO_BIKE, _('e-cargo-bike')),
     )
     NEVER = 0
     ONCE_PER_MONTH = 1
     ONCE_PER_WEEK = 2
     ONCE_PER_DAY = 3
     USAGE_CHOICES = (
-        (NEVER, 'never'),
-        (ONCE_PER_DAY, 'once per day'),
-        (ONCE_PER_WEEK, 'once per week'),
-        (ONCE_PER_MONTH, 'once per month'),
+        (NEVER, _('never')),
+        (ONCE_PER_DAY, _('once per day')),
+        (ONCE_PER_WEEK, _('once per week')),
+        (ONCE_PER_MONTH, _('once per month')),
     )
-    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    age = models.PositiveSmallIntegerField(_('age'), blank=True, null=True)
     category_of_bike = models.CharField(
-        blank=True, null=True, max_length=20, choices=CATEGORY_OF_BIKE_CHOICES)
-    has_trailer = models.NullBooleanField(blank=True, null=True)
+        _('category of bike'),
+        blank=True,
+        null=True,
+        max_length=20,
+        choices=CATEGORY_OF_BIKE_CHOICES
+    )
+    has_trailer = models.NullBooleanField(
+        _('has trailer'), blank=True, null=True
+    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    postal_code = models.CharField(blank=True, null=True, max_length=5)
+    postal_code = models.CharField(
+        _('postal code'), blank=True, null=True, max_length=5
+    )
     sex = models.CharField(
-        blank=True, null=True, max_length=1, choices=SEX_CHOICES)
-    speed = models.PositiveSmallIntegerField(blank=True, null=True)
-    security = models.PositiveSmallIntegerField(blank=True, null=True)
+        _('sex'), blank=True, null=True, max_length=1, choices=SEX_CHOICES
+    )
+    speed = models.PositiveSmallIntegerField(_('speed'), blank=True, null=True)
+    security = models.PositiveSmallIntegerField(
+        _('security'), blank=True, null=True
+    )
     usage = models.PositiveSmallIntegerField(
-        blank=True, null=True, choices=USAGE_CHOICES)
+        _('usage'), blank=True, null=True, choices=USAGE_CHOICES
+    )
+
+    class Meta:
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
 
 
 class Report(BaseModel):
@@ -896,26 +966,35 @@ class Report(BaseModel):
     STATUS_DONE = 'done'
 
     STATUS_CHOICES = (
-        (STATUS_NEW, 'new'),
-        (STATUS_VERIFICATION, 'verification'),
-        (STATUS_ACCEPTED, 'accepted'),
-        (STATUS_REJECTED, 'rejected'),
-        (STATUS_DONE, 'done')
+        (STATUS_NEW, _('new')),
+        (STATUS_VERIFICATION, _('verification')),
+        (STATUS_ACCEPTED, _('accepted')),
+        (STATUS_REJECTED, _('rejected')),
+        (STATUS_DONE, _('done'))
     )
 
-    address = models.TextField(blank=True, null=True)
-    geometry = models.PointField(srid=4326)
-    description = models.CharField(blank=True, null=True, max_length=400)
-    details = JSONField()
+    address = models.TextField(_('address'), blank=True, null=True)
+    geometry = models.PointField(_('geometry'), srid=4326)
+    description = models.CharField(
+        _('description'), blank=True, null=True, max_length=400
+    )
+    details = JSONField(_('details'))
     likes = GenericRelation(Like)
     photo = GenericRelation(Photo)
-    published = models.BooleanField(default=True)
+    published = models.BooleanField(_('published'), default=True)
     status = models.CharField(
+        _('status'),
         blank=True,
         null=True,
         max_length=20,
         choices=STATUS_CHOICES,
         default=STATUS_NEW)
-    status_reason = models.TextField(blank=True, null=True)
+    status_reason = models.TextField(
+        _('reason for status'), blank=True, null=True
+    )
     user = models.ForeignKey(
         get_user_model(), blank=True, null=True, on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = _('report')
+        verbose_name_plural = _('reports')

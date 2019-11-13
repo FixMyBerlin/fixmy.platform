a5ac163dcc2167607daf60935b5bf063f3e03d1a
Stefan Freudenberg <stefan@agaric.com>
2019-11-13 17:40:08 +0100

    Implement request for new scene group

--- survey/views.py
+++ survey/views.py
@@ -4,7 +4,7 @@ from rest_framework import permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Scene, Rating
+from .models import Rating, Scene, Survey
 from .serializers import SurveySerializer
 
 
@@ -48,7 +48,24 @@ class SurveyView(APIView):
 
     @transaction.atomic
     def post(self, request, project, session):
-        pass
+        perspective_ = request.data.get('perspective')
+        perspective = self.perspective_map[perspective_][0]
+        size = self.perspective_map[perspective_][1]
+
+        scenes = Scene.random_group(perspective, project, size)
+        total_ratings = Rating.objects.filter(rating__isnull=False).count()
+        survey = Survey.objects.get(id=session)
+
+        for s in scenes:
+            Rating.objects.create(scene=s, survey=survey)
+
+        return Response(
+            data={
+                'scenes': [str(s) for s in scenes],
+                'ratings_total': total_ratings
+            },
+            status=status.HTTP_200_OK
+        )
 
 
 @api_view(['PUT'])

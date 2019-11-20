972daa1e41dc1ea614673161d6c814b1b7d228f5
Stefan Freudenberg <stefan@agaric.com>
2019-11-20 17:27:20 +0100

    Fix command for importing scenes

--- survey/management/commands/importscenes.py
+++ survey/management/commands/importscenes.py
@@ -29,13 +29,14 @@ class Command(BaseCommand):
 
         for row in islice(reader, 1, None):
             parts = row['SceneID'].split('_')
-            scene, _ = Scene.objects.get_or_create(
+            scene, _ = Scene.objects.update_or_create(
+                defaults={
+                    'weight': row['Weight'],
+                    'image': '{}/{}.jpg'.format(
+                        options['image_dir'], row['SceneID'])
+                },
                 project=parts[0],
                 experiment=parts[1],
                 perspective=parts[2],
-                number=parts[3],
+                number=parts[3]
             )
-            scene.weight = row['Weight']
-            scene.image='{}/{}.jpg'.format(
-                options['image_dir'], row['SceneID'])
-            scene.save()

1f135ab74e277869dee8e948d401a0800e3e1816
Stefan Freudenberg <stefan@agaric.com>
2019-11-27 16:34:41 +0100

    Register Session model with admin

--- survey/admin.py
+++ survey/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from survey.models import Scene
+from survey.models import Scene, Session
 
 
 class SceneAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'weight')
 
 
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_date')
+
+
 admin.site.register(Scene, SceneAdmin)
+admin.site.register(Session, SessionAdmin)

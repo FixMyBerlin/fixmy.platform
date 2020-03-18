b0e724a4c911c36ecef6cf7383cdfdeb54023e7e
Stefan Freudenberg <stefan@agaric.com>
2020-03-18 16:50:56 +0100

    Fix restoring database

--- scripts/fetch_database.sh
+++ scripts/fetch_database.sh
@@ -3,4 +3,4 @@ trap "rm -f latest.dump" EXIT INT TERM
 rm -f latest.dump
 heroku pg:backups:capture -a fixmyplatform-develop
 heroku pg:backups:download -a fixmyplatform-develop
-docker-compose run --rm app bash -c "pg_restore -c -O latest.dump && python manage.py manage.py anonymizedata && python manage.py migrate"
+docker-compose run --rm app bash -c "pg_restore -c -O -d docker latest.dump; python manage.py anonymizedata; python manage.py migrate"

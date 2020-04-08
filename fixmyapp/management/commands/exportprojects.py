88ccff14baf0448630d064ed7faf30baa6e82870
Vincent Ahrend <mail@vincentahrend.com>
2020-04-08 10:23:08 +0200

    Remove filter for only published projects in export

--- fixmyapp/management/commands/exportprojects.py
+++ fixmyapp/management/commands/exportprojects.py
@@ -8,51 +8,43 @@ class Command(BaseCommand):
     help = 'Exports projects as GeoJSON'
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            'file',
-            type=argparse.FileType('w'),
-            help='write to file'
-        )
+        parser.add_argument('file', type=argparse.FileType('w'), help='write to file')
         parser.add_argument(
             '--indent',
             type=int,
             default=None,
-            help='indentation level for pretty printing'
+            help='indentation level for pretty printing',
         )
 
     def handle(self, *args, **options):
-        result = {
-            'type': 'FeatureCollection',
-            'features': []
-        }
+        result = {'type': 'FeatureCollection', 'features': []}
 
-        filters = {
-            'geometry__isnull': False,
-            'published': True
-        }
+        filters = {'geometry__isnull': False}
 
         for p in Project.objects.filter(**filters):
-            result['features'].append({
-                'type': 'Feature',
-                'geometry': json.loads(p.geometry.json),
-                'properties': {
-                    'id': p.pk,
-                    'title': p.title,
-                    'side': p.side,
-                    'responsible': p.responsible,
-                    'short_description': p.short_description,
-                    'category': p.category,
-                    'project_key': p.project_key,
-                    'street_name': p.street_name,
-                    'borough': p.borough,
-                    'costs': p.costs,
-                    'draft_submitted': p.draft_submitted,
-                    'construction_started': p.construction_started,
-                    'construction_completed': p.construction_completed,
-                    'phase': p.phase,
-                    'status': p.status,
-                    'external_url': p.external_url
+            result['features'].append(
+                {
+                    'type': 'Feature',
+                    'geometry': json.loads(p.geometry.json),
+                    'properties': {
+                        'id': p.pk,
+                        'title': p.title,
+                        'side': p.side,
+                        'responsible': p.responsible,
+                        'short_description': p.short_description,
+                        'category': p.category,
+                        'project_key': p.project_key,
+                        'street_name': p.street_name,
+                        'borough': p.borough,
+                        'costs': p.costs,
+                        'draft_submitted': p.draft_submitted,
+                        'construction_started': p.construction_started,
+                        'construction_completed': p.construction_completed,
+                        'phase': p.phase,
+                        'status': p.status,
+                        'external_url': p.external_url,
+                    },
                 }
-            })
+            )
 
         json.dump(result, options['file'], indent=options['indent'])

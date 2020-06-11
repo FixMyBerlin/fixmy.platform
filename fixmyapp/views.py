8b71fdd1e363952af1a5f7044b176d87a6a3d06f
Vincent Ahrend <mail@vincentahrend.com>
2020-06-11 18:44:06 +0200

    Remove unused imports

--- fixmyapp/views.py
+++ fixmyapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.parsers import FileUploadParser, JSONParser, MultiPartParser
+from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import (

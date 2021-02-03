830f84c890c115aa3cd829cb317bf675cabcfec2
Vincent Ahrend <mail@vincentahrend.com>
2021-02-03 15:11:02 +0000

    Add SectionAccidents model

---
+++ fixmyapp/models/section_accidents.py
@@ -0,0 +1,34 @@
+from django.contrib.gis.db import models
+from django.utils.translation import gettext_lazy as _
+
+from .base_model import BaseModel
+from .section import Section
+
+
+class SectionAccidents(BaseModel):
+    """Accident data for a section."""
+
+    section = models.ForeignKey(
+        Section, related_name='accidents', on_delete=models.CASCADE
+    )
+
+    killed = models.IntegerField(_('killed'))
+    severely_injured = models.IntegerField(_('severely injured'))
+    slightly_injured = models.IntegerField(_('slightly injured'))
+    source = models.TextField(_('source'), blank=True, null=True)
+
+    RISK_LEVEL_CHOICES = (
+        (2, _('accident blackspot')),
+        (1, _('some accidents')),
+        (0, _('no accidents')),
+    )
+    risk_level = models.PositiveSmallIntegerField(
+        _('risk level'), choices=RISK_LEVEL_CHOICES
+    )
+
+    class Meta:
+        verbose_name = _('Section accident data')
+        verbose_name_plural = _('Section accident datasets')
+
+    def __str__(self):
+        return f"{_('Section accident data')} {str(self.section)}"

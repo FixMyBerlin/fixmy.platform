d4cbf0bc0f2b8faa1e715384ee27aa34e8f12904
Vincent Ahrend <mail@vincentahrend.com>
2021-05-20 10:56:49 +0200

    Remove permit toggles in favor of setting explicit start and end times (#571)
    * Remove permit toggles in favor of setting explicit start and end times
    
    * Add documentation

--- permits/utils.py
+++ permits/utils.py
@@ -5,9 +5,11 @@ from django.conf import settings
 
 
 def event_signups_open():
-    """Return True if event signups are configured to be open."""
-    if settings.TOGGLE_EVENT_SIGNUPS in [True, False]:
-        return settings.TOGGLE_EVENT_SIGNUPS
+    """Return True if event signups are configured to be open.
+
+    If no valid date is set in the open and close env vars, this defaults to False"""
+    if settings.EVENT_SIGNUPS_OPEN == None or settings.EVENT_SIGNUPS_CLOSE == None:
+        return True
 
     try:
         start = dateutil.parser.parse(settings.EVENT_SIGNUPS_OPEN).replace(
@@ -16,11 +18,11 @@ def event_signups_open():
         end = dateutil.parser.parse(settings.EVENT_SIGNUPS_CLOSE).replace(
             tzinfo=timezone.utc
         )
-    except TypeError:
+    except dateutil.parser._parser.ParserError:
         # No explicit start and end times defined
         sys.stderr.write(
             'Error parsing event signup open and close times. Fallback to open.'
         )
-        return True
+        return False
     rv = start < datetime.now(tz=timezone.utc) < end
     return rv

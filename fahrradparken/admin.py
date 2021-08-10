2367b1fd20734f3abf935efb0a44df27e2c576d7
Vincent Ahrend <mail@vincentahrend.com>
2021-08-10 14:03:08 +0000

    Add station admin

--- fahrradparken/admin.py
+++ fahrradparken/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Signup, EventSignup
+from .models import Signup, EventSignup, Station
 
 
 class SignupAdmin(admin.ModelAdmin):
@@ -11,5 +11,10 @@ class EventSignupAdmin(admin.ModelAdmin):
     list_display = ('event_title', 'email')
 
 
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'community')
+
+
 admin.site.register(Signup, SignupAdmin)
 admin.site.register(EventSignup, EventSignupAdmin)
+admin.site.register(Station, StationAdmin)

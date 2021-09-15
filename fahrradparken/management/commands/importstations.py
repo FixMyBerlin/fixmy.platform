c84237690c18c1818d58331485b98e2a385a3de7
Vincent Ahrend <mail@vincentahrend.com>
2021-09-15 17:12:09 +0200

    Update stations using import command (#605)
    * Let command update stations
    
    * Let command download files from fixed location
    
    * Add optional argument to set dataset location

--- fahrradparken/management/commands/importstations.py
+++ fahrradparken/management/commands/importstations.py
@@ -2,7 +2,8 @@ import json
 import os
 
 from django.contrib.gis.geos import Point
-from django.core.management.base import BaseCommand
+from django.core.files.storage import default_storage
+from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 
 from fahrradparken.models import Station
@@ -18,13 +19,20 @@ TRAVELLER_COUNT_RANGES = {
     "über 50000": 7,
 }
 
+# Path at which the dataset is stored in S3
+STORAGE_PATH = 'Data/stations-v1.0.geojson'
+
 
 class Command(BaseCommand):
-    help = 'Imports train station dataset'
+    help = 'Import train station dataset from S3 storage'
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'file', type=str, help='A geojson file containing a station dataset'
+            'file',
+            type=str,
+            default='',
+            nargs='?',  # make this optional
+            help='A geojson file containing a station dataset (optional)',
         )
 
     def validate(self, feature):
@@ -67,13 +75,34 @@ class Command(BaseCommand):
         """Return true if this station serves light trail trains."""
         return feature["properties"]["Produktlinie\n(Stand: 03.03.2021)"] == 'S-Bahnhof'
 
+    def download_dataset(self):
+        """Download stations dataset from storage."""
+        TEMP_PATH = '/tmp/stations.geojson'
+
+        if not default_storage.exists(STORAGE_PATH):
+            raise CommandError(f'Dataset not found at path: {STORAGE_PATH}')
+
+        self.stdout.write(f'Downloading dataset from {STORAGE_PATH}')
+        default_storage.bucket.download_file(STORAGE_PATH, TEMP_PATH)
+        try:
+            with open(TEMP_PATH) as f:
+                data = json.load(f)
+        except:
+            self.stderr.write('Error decoding dataset loaded from storage')
+            raise
+        return data
+
     def handle(self, *args, **kwargs):
-        path = os.path.abspath(kwargs['file'])
-        with open(path) as f:
-            data = json.load(f)
+        if kwargs['file'] == '':
+            data = self.download_dataset()
+        else:
+            path = os.path.abspath(kwargs['file'])
+            with open(path) as f:
+                data = json.load(f)
 
         num_entries = len(data.get('features'))
-        num_updates = 0
+        num_created = 0
+        num_updated = 0
 
         with transaction.atomic():
             for i, feature in enumerate(data.get('features', [])):
@@ -103,10 +132,19 @@ class Command(BaseCommand):
                         community=props.get('Gemeindename'),
                     )
                     instance.save()
+                    num_created += 1
                 else:
-                    num_updates += 1
+                    instance.name = props.get('Bahnhof')
+                    instance.location = Point(feature["geometry"]["coordinates"])
+                    instance.travellers = travellers
+                    instance.post_code = props.get('PLZ')
+                    instance.is_long_distance = self.is_long_distance(feature)
+                    instance.is_light_rail = self.is_light_rail(feature)
+                    instance.is_subway = False  # not supported yet
+                    instance.community = props.get('Gemeindename')
+                    instance.save()
+                    num_updated += 1
 
-            if num_updates > 0:
-                self.stdout.write(
-                    f'Updating station data is not supported (ignored {num_updates} entries)'
-                )
+            self.stdout.write(f'Created {num_created} stations')
+            if num_updated > 0:
+                self.stdout.write(f'Updated {num_updated} stations')

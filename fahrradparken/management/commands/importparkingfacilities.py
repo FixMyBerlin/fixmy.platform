4079f342a8820b162cc65c03ba6f5a2d7c9bf17f
Stefan Freudenberg <stefan@agaric.com>
2021-12-01 10:58:55 +0100

    Improvements to parking facility import (#623)
    * Improve command for importing parking facilities
    
    * Make capacity optional

--- fahrradparken/management/commands/importparkingfacilities.py
+++ fahrradparken/management/commands/importparkingfacilities.py
@@ -19,7 +19,6 @@ logger = logging.getLogger(__name__)
 class Command(BaseCommand):
     help = 'Import parking facility dataset from S3 storage'
     fields = [
-        'station_id',
         'external_id',
         'capacity',
         'stands',
@@ -41,11 +40,17 @@ class Command(BaseCommand):
         for row in reader:
             condition = row.get('condition')
             occupancy = row.get('occupancy')
-            coordinates = row.get('location', '').split(',')
+            coordinates = row.get('location', '').split(', ')
+            if len(coordinates) != 2:
+                logger.warning(
+                    f"Skipped importing parking facility {row['external_id']} due to malformed or missing coordinates"
+                )
+                continue
             longitude = float(coordinates[1].strip().replace(',', '.'))
             latitude = float(coordinates[0].strip().replace(',', '.'))
             kwargs = {k: row[k] if row[k] != '' else None for k in self.fields}
             kwargs['location'] = Point(longitude, latitude)
+            kwargs['station_id'] = kwargs['external_id'].split('.')[0]
             try:
                 created = ParkingFacility.objects.create(**kwargs)
                 if condition:
@@ -60,12 +65,12 @@ class Command(BaseCommand):
                 if type(e.__cause__) == psycopg2.errors.ForeignKeyViolation:
                     logger.warning(
                         'Skipped importing parking facility for missing station '
-                        f"{row['station_id']}"
+                        f"{kwargs['station_id']}"
                     )
                 elif type(e.__cause__) == psycopg2.errors.UniqueViolation:
                     logger.warning(
                         'Skipped importing duplicate parking facility '
-                        f"{row['external_id']} of station {row['station_id']}"
+                        f"{row['external_id']}"
                     )
                 else:
                     raise

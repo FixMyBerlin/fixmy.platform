e289f65dd024a442618b6702b71c07a47846ae4d
Laurenz Rasche <laurenz.rasche@gmail.com>
2022-07-12 16:44:06 +0200

    `ReadOnlySchemaGenerator` class in sperate file

---
+++ fahrradparken/openapi.py
@@ -0,0 +1,15 @@
+from rest_framework.schemas.openapi import SchemaGenerator
+
+# class to modify the generated schema s.t. there are only get methods
+class ReadOnlySchemaGenerator(SchemaGenerator):
+    def get_schema(self, *args, **kwargs):
+        schema = super().get_schema(*args, **kwargs)
+        paths = schema['paths']
+        print(paths)
+        ro_paths = {}
+        for p in paths:
+            if 'get' in paths[p]:
+                ro_paths[p] = {'get': paths[p]['get']}
+        print(ro_paths)
+        schema['paths'] = ro_paths
+        return schema
